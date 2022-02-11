f86866c59a7fd1c5d6866616f085d738db421d9b
tyrion98 <tyrionw@gmail.com>
2022-02-11 14:10:58 -0500

    Finished the project, added a eraser button, added the ability for a user to pick whether they want the grid to be small, medium, or large

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,15 +1,28 @@
 
+let isEraser = false;
+// clear divs
+function clearDivs() {
+
+  // vars
+  // get main div container
+  const gridContainer = document.querySelector(".grid-container");
+
+  while(gridContainer.firstChild) {
+
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
 
 
+}
 // create 16x16 grid of square divs
-function createDivs() {
+function createDivs(n) {
   // vars
   let i, j = 0;
   // get main div container
   const gridContainer = document.querySelector(".grid-container");
 
-  for(i = 0; i < 16; i++){
-    for(j = 0; j < 16; j++){
+  for(i = 0; i < n; i++){
+    for(j = 0; j < n; j++){
         // create divs
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-item");
@@ -22,8 +35,27 @@ function createDivs() {
 
 }
 
+// erase button
+const eraseBtn = document.querySelector(".erase-button");
+// listen to button click
+eraseBtn.addEventListener("click", onErasePressed);
+// erase mouseHover
+
+function onErasePressed(){
+
+  if(isEraser){
+    isEraser = false;
+    eraseBtn.style.backgroundColor = "#2D4263";
+
+  }else{
+    isEraser = true;
+    eraseBtn.style.backgroundColor = "#2F4F4F";
+  }
+  mouseHover(isEraser);
+}
+
 // checks if a grid item has been clicked
-function mouseHover() {
+function mouseHover(isEraser) {
   // vars
   let isMouseHover = false;
   const gridContainer = document.querySelector(".grid-container");
@@ -33,22 +65,40 @@ function mouseHover() {
 
     listOfDivs[i].addEventListener("mouseover", function() {
       isMouseHover = true;
-      listOfDivs[i].style.backgroundColor = "#191919";
+      if (!isEraser){
+        listOfDivs[i].style.backgroundColor = "#191919";
+      } else if(isEraser){
+        listOfDivs[i].style.backgroundColor = "#ECDBBA";
+      }
 
     });
+  }
+}
 
+// remove class
+function removeClass(gridType, gridContainer){
 
+  switch(gridType){
+    case "small":
+      gridContainer.classList.remove("big-grid");
+      gridContainer.classList.remove("normal-grid");
+      break;
+
+    case "normal":
+      gridContainer.classList.remove("big-grid");
+      gridContainer.classList.remove("small-grid");
+      break;
+
+      case "big":
+        gridContainer.classList.remove("small-grid");
+        gridContainer.classList.remove("normal-grid");
+        break;
   }
 
-}
-
-// color change
-function changeColor () {
-
-  listOfDivs[i].style.backgroundColor = "#191919";
 
 }
 
+// clear grid
 const clearBtn = document.querySelector(".clear-button");
 // listen to button click
 clearBtn.addEventListener("click", onClearButtonClick);
@@ -64,17 +114,54 @@ function onClearButtonClick(){
   }
 }
 
-const newBtn = document.querySelector(".new-button");
+// big grid
+const bigBtn = document.querySelector(".big-button");
 // listen to button click
-newBtn.addEventListener("click", onNewButtonClick);
+bigBtn.addEventListener("click", onBigButtonClick);
 // 100x100 grid
-function onNewButtonClick(){
-  console.log("clicked");
+function onBigButtonClick(){
   const gridContainer = document.querySelector(".grid-container");
+  removeClass("big", gridContainer);
   gridContainer.classList.add("big-grid");
+  clearDivs();
+  createDivs(8);
+  mouseHover();
+
 
 }
 
+// normal button
+const normalBtn = document.querySelector(".normal-button");
+// listen to button click
+normalBtn.addEventListener("click", onNormalButtonClick);
+// 100x100 grid
+function onNormalButtonClick(){
+  console.log("clicked");
+  const gridContainer = document.querySelector(".grid-container");
+  removeClass("normal", gridContainer);
+  gridContainer.classList.add("normal-grid");
+  clearDivs();
+  createDivs(16);
+  mouseHover();
+
+}
+
+// Small button
+const smallBtn = document.querySelector(".small-button");
+// listen to button click
+smallBtn.addEventListener("click", onSmallButtonClick);
+// 100x100 grid
+function onSmallButtonClick(){
+  const gridContainer = document.querySelector(".grid-container");
+  removeClass("small", gridContainer);
+  gridContainer.classList.add("small-grid");
+  clearDivs();
+  createDivs(64);
+  mouseHover();
+
+}
+
+
 // call createDivs function
-createDivs();
+createDivs(16);
 mouseHover();
